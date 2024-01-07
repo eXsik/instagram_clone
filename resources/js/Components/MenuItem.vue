fc7e90306a7864c4c2ebc995ed9a6a04d8bab5b0
eXsik <qba.skupinski@gmail.com>
2024-01-07 19:59:11 +0100

    Working of main layout

---
+++ resources/js/Components/MenuItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { toRefs } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <input
+        type="checkbox"
+        :value="value"
+        v-model="proxyChecked"
+        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+    />
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout>
+        <div class="bg-gray-600 min-h-[100vh] text-white">
+            HOME.VUE
+        </div>
+    </MainLayout>
+</template>
